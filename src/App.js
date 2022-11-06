5a6c436ce5d3a1a1d48798480a1d0bb039287233
Janindu Gayanga <janindugayanga10@gmail.com>
2022-11-06 19:14:07 +0530

    chartjs2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+
+}from "chart.js";
+import { Bar} from "react-chartjs-2";
+import React, {useState, useEffect } from "react";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function App() {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+const [chartOptions, setChartOptions] = useState({});
+
+useEffect(() => {
+  setChartData({
+    labels: ["Janindu","Chathura","Ravindu","Kamal"],
+    datasets: [
+      {
+        label: "Whom'st let the dogs out",
+        data: [500,450,700,120,720],
+        borderColor: "rgb(53,162,235)",
+        backgroundColor: "black",
+      },
+    ],
+  });
+  setChartOptions({
+    responsive: true,
+    Plugins: {
+      Legend: {
+        position: "top"
+      },
+      title: {
+        display: true,
+        color: "red",
+        text: "Whom'st let the dogs out",
+      },
+    },
+  });
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bar options={chartOptions} data={chartData}/>
+  
     </div>
   );
 }
